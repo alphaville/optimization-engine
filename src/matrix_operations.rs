47a800833ba718ae5ceed9681bc67535cea259f2
Emil Fresk <emil.fresk@gmail.com>
2018-11-25 19:39:59 +0100

    README + license + matrix_operations

---
+++ src/matrix_operations.rs
@@ -0,0 +1,78 @@
+//! # matrix_operations
+//!
+//! Matrix operations used by the optimization algorithm.
+//!
+//! # Examples
+//!
+//! ```
+//! fn main() {
+//! }
+//! ```
+//!
+//! # Errors
+//!
+//!
+//! # Panics
+//!
+//!
+
+/// Calculate the inner product of two vectors
+pub fn inner_product(a: &Vec<f64>, b: &Vec<f64>) -> f64 {
+    assert!(a.len() == b.len());
+
+    a.iter().zip(b.iter()).map(|(x, y)| x * y).sum()
+}
+
+/// Calculate the 2-norm of a vector
+pub fn norm1(a: &Vec<f64>) -> f64 {
+    a.iter().map(|x| x.abs()).sum()
+}
+
+/// Calculate the 2-norm of a vector
+pub fn norm2(a: &Vec<f64>) -> f64 {
+    let norm: f64 = a.iter().map(|x| x * x).sum();
+    norm.sqrt()
+}
+
+/// Calculate the infinity-norm of a vector
+pub fn norm_inf(a: &Vec<f64>) -> f64 {
+    a.iter().fold(0.0, |max, x| x.abs().max(max))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn inner_product_test() {
+        assert_eq!(
+            matrix_operations::inner_product(&vec![1.0, 2.0, 3.0], &vec![1.0, 2.0, 3.0]),
+            14.0
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn inner_product_test_panic() {
+        matrix_operations::inner_product(&vec![2.0, 3.0], &vec![1.0, 2.0, 3.0]);
+    }
+
+    #[test]
+    fn norm1_test() {
+        assert_eq!(matrix_operations::norm1(&vec![1.0, -2.0, -3.0]), 6.0);
+    }
+
+    #[test]
+    fn norm2_test() {
+        assert_eq!(matrix_operations::norm2(&vec![3.0, 4.0]), 5.0);
+    }
+
+    #[test]
+    fn norm_inf_test() {
+        assert_eq!(matrix_operations::norm_inf(&vec![1.0, -2.0, -3.0]), 3.0);
+        assert_eq!(
+            matrix_operations::norm_inf(&vec![1.0, -8.0, -3.0, 0.0]),
+            8.0
+        );
+    }
+}
