a3d28467ec1166fe6005f5208c8f63d3826ea7a5
Pantelis Sopasakis <p.sopasakis@gmail.com>
2018-12-05 03:02:59 +0200

    :hammer: refactoring

--- src/optimizer.rs
+++ src/optimizer.rs
@@ -5,6 +5,8 @@ use crate::proximal_gradient_descent::ProjectedGradient;
 static MAX_ITER: usize = 100_usize;
 static TOLERANCE: f64 = 1e-4;
 
+/* ---------------------------------------------------------------------------- */
+
 pub struct SolverStatus {
     pub converged: bool,
     pub num_iter: usize,
@@ -21,21 +23,19 @@ impl SolverStatus {
     }
 }
 
-pub struct Optimizer<GradientType, ConstraintType>
+/* ---------------------------------------------------------------------------- */
+
+pub struct Problem<GradientType, ConstraintType>
 where
     GradientType: Fn(&[f64], &mut [f64]) -> i32,
     ConstraintType: constraints::Constraint,
 {
-    constraints: ConstraintType,
-    gradf: GradientType,
-    work_gradient_u: Vec<f64>,
-    work_u_previous: Vec<f64>,
-    gamma: f64,
-    max_iter: usize,
-    epsilon: f64,
+    pub constraints: ConstraintType,
+    pub gradf: GradientType,
+    pub n: usize,
 }
 
-impl<GradientType, ConstraintType> Optimizer<GradientType, ConstraintType>
+impl<GradientType, ConstraintType> Problem<GradientType, ConstraintType>
 where
     GradientType: Fn(&[f64], &mut [f64]) -> i32,
     ConstraintType: constraints::Constraint,
@@ -44,25 +44,61 @@ where
         c_: ConstraintType,
         g_: GradientType,
         len: usize,
-    ) -> Optimizer<GradientType, ConstraintType> {
-        Optimizer {
+    ) -> Problem<GradientType, ConstraintType> {
+        Problem {
             constraints: c_,
             gradf: g_,
-            work_gradient_u: vec![0.0; len],
-            work_u_previous: vec![0.0; len],
+            n: len,
+        }
+    }
+}
+
+/* ---------------------------------------------------------------------------- */
+
+pub struct FBSOptimizer<'a, GradientType, ConstraintType>
+where
+    GradientType: Fn(&[f64], &mut [f64]) -> i32 + 'a,
+    ConstraintType: constraints::Constraint + 'a,
+{
+    problem: &'a Problem<GradientType, ConstraintType>,
+    work_gradient_u: Vec<f64>,
+    work_u_previous: Vec<f64>,
+    gamma: f64,
+    max_iter: usize,
+    epsilon: f64,
+}
+
+impl<'a, GradientType, ConstraintType> FBSOptimizer<'a, GradientType, ConstraintType>
+where
+    GradientType: Fn(&[f64], &mut [f64]) -> i32 + 'a,
+    ConstraintType: constraints::Constraint + 'a,
+{
+    pub fn new(
+        problem: &'a Problem<GradientType, ConstraintType>,
+    ) -> FBSOptimizer<GradientType, ConstraintType> {
+        FBSOptimizer {
+            problem: problem,
+            work_gradient_u: vec![0.0; problem.n],
+            work_u_previous: vec![0.0; problem.n],
             gamma: 0.1_f64,
             max_iter: MAX_ITER,
             epsilon: TOLERANCE,
         }
     }
 
-    pub fn with_epsilon(&mut self, epsilon: f64) -> &mut Optimizer<GradientType, ConstraintType> {
+    pub fn with_epsilon(
+        &mut self,
+        epsilon: f64,
+    ) -> &mut FBSOptimizer<'a, GradientType, ConstraintType> {
         assert!(epsilon > 0.0);
         self.epsilon = epsilon;
         self
     }
 
-    pub fn with_gamma(&mut self, gamma: f64) -> &mut Optimizer<GradientType, ConstraintType> {
+    pub fn with_gamma(
+        &mut self,
+        gamma: f64,
+    ) -> &mut FBSOptimizer<'a, GradientType, ConstraintType> {
         assert!(gamma > 0.0);
         self.gamma = gamma;
         self
@@ -71,14 +107,17 @@ where
     pub fn with_max_iter(
         &mut self,
         max_iter: usize,
-    ) -> &mut Optimizer<GradientType, ConstraintType> {
+    ) -> &mut FBSOptimizer<'a, GradientType, ConstraintType> {
         self.max_iter = max_iter;
         self
     }
 
     pub fn solve(&mut self, u: &mut [f64]) -> SolverStatus {
-        let mut pg_method =
-            ProjectedGradient::new(&self.gradf, &self.constraints, &mut self.work_gradient_u);
+        let mut pg_method = ProjectedGradient::new(
+            &self.problem.gradf,
+            &self.problem.constraints,
+            &mut self.work_gradient_u,
+        );
         self.work_u_previous.copy_from_slice(u);
         let mut norm_fpr = std::f64::INFINITY;
         let mut num_iter = 0_usize;
@@ -92,6 +131,9 @@ where
     }
 }
 
+/* ---------------------------------------------------------------------------- */
+/*          TESTS                                                               */
+/* ---------------------------------------------------------------------------- */
 #[cfg(test)]
 mod tests {
 
@@ -107,13 +149,14 @@ mod tests {
     #[test]
     fn solve_problem() {
         let box_constraints = constraints::Ball2::new_at_origin_with_radius(0.2);
-        let mut problem = Optimizer::new(box_constraints, my_gradient, 2);
-        problem
+        let problem = Problem::new(box_constraints, my_gradient, 2);
+        let mut optimizer = FBSOptimizer::new(&problem);
+        optimizer
             .with_epsilon(1e-4)
             .with_gamma(0.1)
             .with_max_iter(100);
         let mut u_init = [1.0, -0.5];
-        let status = problem.solve(&mut u_init);
+        let status = optimizer.solve(&mut u_init);
         assert!(status.converged);
         println!("log |fpr| = {:.2}", status.fpr_norm.log10());
     }
