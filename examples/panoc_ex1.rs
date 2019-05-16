db2e3b9fcc83e06562622093b679424c267dff55
Emil Fresk <emil.fresk@gmail.com>
2019-05-16 07:19:14 +0200

    First example added

--- examples/panoc_ex1.rs
+++ examples/panoc_ex1.rs
@@ -1,6 +1,13 @@
-use optimization_engine::constraints::*;
-use optimization_engine::core::panoc::*;
-use optimization_engine::core::*;
+//! # PANOC Example 1
+//!
+//! This example shows how to solve an optimization problem based on the [Rosenbrock function].
+//!
+//! [Rosenbrock function]: https://en.wikipedia.org/wiki/Rosenbrock_function
+
+use optimization_engine::{
+    constraints::*,
+    core::{panoc::*, *},
+};
 use std::num::NonZeroUsize;
 
 fn rosenbrock_cost(a: f64, b: f64, u: &[f64]) -> f64 {
@@ -17,8 +24,8 @@ fn main() {
     let tolerance = 1e-14;
     let a = 1.0;
     let b = 200.0;
-    let n = 2;
-    let lbfgs_memory = 10;
+    let problem_size = NonZeroUsize::new(2).unwrap();
+    let lbfgs_memory = NonZeroUsize::new(10).unwrap();
     let max_iters = 80;
     let mut u = [-1.5, 0.9];
     let radius = 1.0;
@@ -28,6 +35,7 @@ fn main() {
         rosenbrock_grad(a, b, u, grad);
         0
     };
+
     let f = |u: &[f64], c: &mut f64| -> i32 {
         *c = rosenbrock_cost(a, b, u);
         0
@@ -38,11 +46,7 @@ fn main() {
 
     /* PROBLEM STATEMENT */
     let problem = Problem::new(bounds, df, f);
-    let mut panoc_cache = PANOCCache::new(
-        NonZeroUsize::new(n).unwrap(),
-        tolerance,
-        NonZeroUsize::new(lbfgs_memory).unwrap(),
-    );
+    let mut panoc_cache = PANOCCache::new(problem_size, tolerance, lbfgs_memory);
     let mut panoc_engine = PANOCEngine::new(problem, &mut panoc_cache);
     let mut panoc = PANOCOptimizer::new(&mut panoc_engine);
     panoc.with_max_iter(max_iters);
@@ -50,5 +54,6 @@ fn main() {
     // Invoke the solver
     let status = panoc.solve(&mut u);
 
-    println!("Panoc solution: {:?}", status);
+    println!("Panoc status: {:#?}", status);
+    println!("Panoc solution: {:#?}", u);
 }
