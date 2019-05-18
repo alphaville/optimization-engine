1e9611c421df6d487379e90ee068407ff9eea0ee
Emil Fresk <emil.fresk@gmail.com>
2019-05-18 18:44:14 +0200

    Added so both C and C++ headers are generated

--- open-clib/build.rs
+++ open-clib/build.rs
@@ -5,11 +5,19 @@ fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
     Builder::new()
-        .with_crate(crate_dir)
+        .with_crate(&crate_dir)
         .with_language(Language::C)
         .generate()
         .expect("Unable to generate bindings")
         .write_to_file("open_clib.h");
 
+    Builder::new()
+        .with_crate(&crate_dir)
+        .with_language(Language::Cxx)
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file("open_clib.hpp");
+
     println!("cargo:rerun-if-changed=open_clib.h");
+    println!("cargo:rerun-if-changed=open_clib.hpp");
 }
