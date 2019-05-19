fae5419774522e27afcf99cb8167ecc17fcfaeea
Emil Fresk <emil.fresk@gmail.com>
2019-05-19 23:25:06 +0200

    Moved the number of variables and parameters to compile time constants

--- icasadi/extern/icasadi.h
+++ icasadi/extern/icasadi.h
@@ -4,33 +4,27 @@
 #include "icasadi_config.h"
 
 extern int CASADI_COST_NAME(
-        const double** arg, 
-        double** casadi_results, 
-        long long int* iw, 
-        double* w, 
+        const double** arg,
+        double** casadi_results,
+        long long int* iw,
+        double* w,
         void* mem);
 
 extern int CASADI_GRAD_NAME(
-        const double** arg, 
-        double** casadi_results, 
-        long long int* iw, 
-        double* w, 
+        const double** arg,
+        double** casadi_results,
+        long long int* iw,
+        double* w,
         void* mem);
 
 int icasadi_cost_(
-        const double *u, 
+        const double *u,
         const double *casadi_static_params,
         double* cost_value);
 
 int icasadi_grad_(
-        const double *u, 
+        const double *u,
         const double *casadi_static_params,
         double* gradient);
-
-int icasadi_num_decision_variables(void);
-
-int icasadi_num_static_parameters(void);
-
-
 
 #endif
