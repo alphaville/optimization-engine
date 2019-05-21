3408fda7ec317e99a34724d0756eb749f08a59de
Emil Fresk <emil.fresk@gmail.com>
2019-05-21 08:45:20 +0200

    Updated the codegen

--- open-clib/build.rs
+++ open-clib/build.rs
@@ -5,15 +5,25 @@ use std::env;
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
+    let header: &str = concat!(
+        "/* This is an auto-generated file made from optimization engine: ",
+        "https://crates.io/crates/optimization_engine */\n\n",
+        "/** This is the size of all the arrays that the solver needs, except params. */\n",
+        format!(
+            "#define OPEN_NUM_DECISION_VARIABLES {}\n\n",
+            icasadi::NUM_DECISION_VARIABLES,
+        ),
+        "/** This is the size of the param arrays that the solver needs. */\n",
+        format!(
+            "#define OPEN_NUM_STATIC_PARAMETERS {}\n",
+            icasadi::NUM_STATIC_PARAMETERS
+        )
+    );
+
     Builder::new()
         .with_crate(&crate_dir)
         .with_language(Language::C)
-        .with_include_guard("_OPEN_GUARD_")
-        .with_header(format!(
-            "#define OPEN_NUM_DECISION_VARIABLES {}\n#define OPEN_NUM_STATIC_PARAMETERS {}",
-            icasadi::NUM_DECISION_VARIABLES,
-            icasadi::NUM_STATIC_PARAMETERS
-        )) // Sneaky way of getting around cbindgen limitation
+        .with_header(&header)
         .generate()
         .expect("Unable to generate bindings")
         .write_to_file("open_clib.h");
@@ -21,12 +31,7 @@ fn main() {
     Builder::new()
         .with_crate(&crate_dir)
         .with_language(Language::Cxx)
-        .with_include_guard("_OPEN_GUARD_")
-        .with_header(format!(
-            "#define OPEN_NUM_DECISION_VARIABLES {}\n#define OPEN_NUM_STATIC_PARAMETERS {}",
-            icasadi::NUM_DECISION_VARIABLES,
-            icasadi::NUM_STATIC_PARAMETERS
-        )) // Sneaky way of getting around cbindgen limitation
+        .with_header(&header)
         .generate()
         .expect("Unable to generate bindings")
         .write_to_file("open_clib.hpp");
