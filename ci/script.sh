701552a9b69a17aa21fcc06baa037ace6d229722
Pantelis Sopasakis <p.sopasakis@gmail.com>
2019-05-26 20:02:44 +0100

    using explicit py version

--- ci/script.sh
+++ ci/script.sh
@@ -3,8 +3,9 @@ set -euxo pipefail
 main() {
     cargo check --target $TARGET
     cargo test --target $TARGET
-    cd open-codegen    
-    virtualenv venv
+    cd open-codegen 
+    set -euox pipefail
+    virtualenv -p python3.4 venv
     ls
     source venv/bin/activate
     python setup.py install
