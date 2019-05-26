cf9f592dedf99b211039df192f6ef9771b3090fc
Pantelis Sopasakis <p.sopasakis@gmail.com>
2019-05-26 19:47:39 +0100

    using venv in script.sh

--- ci/script.sh
+++ ci/script.sh
@@ -3,6 +3,11 @@ set -euxo pipefail
 main() {
     cargo check --target $TARGET
     cargo test --target $TARGET
+    cd open-codegen
+    pip install virtualenv
+    virtualenv venv
+    source venv/bin/activate
+    python setup.py install
 }
 
 main
