3816448bcaa7278b0f1f87f60255567320d53410
Pantelis Sopasakis <p.sopasakis@gmail.com>
2019-05-26 19:56:03 +0100

    debugging travis ci (python)

--- ci/script.sh
+++ ci/script.sh
@@ -4,7 +4,6 @@ main() {
     cargo check --target $TARGET
     cargo test --target $TARGET
     cd open-codegen    
-    pip install --upgrade pip
     virtualenv venv
     ls
     source venv/bin/activate
