00531fa261d0df02c9b22c2fc842bd07dc9fc4a0
Pantelis Sopasakis <p.sopasakis@gmail.com>
2019-05-26 19:52:00 +0100

    debugging travis ci (python)

--- ci/script.sh
+++ ci/script.sh
@@ -3,9 +3,10 @@ set -euxo pipefail
 main() {
     cargo check --target $TARGET
     cargo test --target $TARGET
-    cd open-codegen
-    pip install virtualenv
+    cd open-codegen    
+    pip install --upgrade pip
     virtualenv venv
+    ls
     source venv/bin/activate
     python setup.py install
 }
