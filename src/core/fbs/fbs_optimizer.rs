398f32c64115c486dcc6e8f94667453414f9ffe9
Pantelis Sopasakis <p.sopasakis@gmail.com>
2019-05-29 21:08:36 +0100

    :hammer: FBSOptimizer: catching all errors propagating failure upstream using?

--- src/core/fbs/fbs_optimizer.rs
+++ src/core/fbs/fbs_optimizer.rs
@@ -95,51 +95,39 @@ where
     fn solve(&mut self, u: &mut [f64]) -> Result<SolverStatus, SolverError> {
         let now = time::Instant::now();
 
-        if let Ok(_) = self.fbs_engine.init(u) {
-            let mut num_iter: usize = 0;
-            if let Some(dur) = self.max_duration {
-                while self.fbs_engine.step(u) == Ok(true)
-                    && num_iter < self.max_iter
-                    && dur <= now.elapsed()
-                {
-                    num_iter += 1;
-                }
-            } else {
-                while self.fbs_engine.step(u) == Ok(true) && num_iter < self.max_iter {
-                    num_iter += 1;
-                }
+        // Initialize - propagate error upstream, if any
+        self.fbs_engine.init(u)?;
+
+        let mut num_iter: usize = 0;
+        let mut step_flag = self.fbs_engine.step(u)?;
+
+        if let Some(dur) = self.max_duration {
+            while step_flag && num_iter < self.max_iter && dur <= now.elapsed() {
+                num_iter += 1;
+                step_flag = self.fbs_engine.step(u)?
             }
-
-            // cost at the solution
-            let mut cost_value = 0.0;
-
-            assert_eq!(
-                Ok(()),
-                (self.fbs_engine.problem.cost)(u, &mut cost_value),
-                "The computation of the cost value at the solution failed"
-            );
-
-            // export solution status
-            Ok(SolverStatus::new(
-                if num_iter < self.max_iter {
-                    ExitStatus::Converged
-                } else {
-                    ExitStatus::NotConvergedIterations
-                },
-                num_iter,
-                now.elapsed(),
-                self.fbs_engine.cache.norm_fpr,
-                cost_value,
-            ))
         } else {
-            // The cost function failed somewhere
-            Ok(SolverStatus::new(
-                ExitStatus::NotConvergedIterations,
-                0,
-                now.elapsed(),
-                std::f64::INFINITY,
-                std::f64::INFINITY,
-            ))
+            while step_flag && num_iter < self.max_iter {
+                num_iter += 1;
+                step_flag = self.fbs_engine.step(u)?
+            }
         }
+
+        // cost at the solution [propagate error upstream]
+        let mut cost_value = 0.0;
+        (self.fbs_engine.problem.cost)(u, &mut cost_value)?;
+
+        // export solution status
+        Ok(SolverStatus::new(
+            if num_iter < self.max_iter {
+                ExitStatus::Converged
+            } else {
+                ExitStatus::NotConvergedIterations
+            },
+            num_iter,
+            now.elapsed(),
+            self.fbs_engine.cache.norm_fpr,
+            cost_value,
+        ))
     }
 }
