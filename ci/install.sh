f05f6f6e1685b9ef96dd08142c81b13d3e146b0c
Pantelis Sopasakis <p.sopasakis@gmail.com>
2019-05-31 03:31:17 +0100

    :heart: debugging travis.yml

--- ci/install.sh
+++ ci/install.sh
@@ -4,12 +4,20 @@ main() {
     if [ $TARGET != x86_64-unknown-linux-gnu ]; then
         rustup target add $TARGET
     fi
-    sudo pip install --upgrade pip
-    sudo pip install virtualenv --upgrade
+    #sudo pip install --upgrade pip
+    #sudo pip install virtualenv --upgrade
     python --version
     pip --version
     virtualenv --version
     which python3
+    export P3=`which python3`
+    if [ ! -n "$P3" ]
+    then
+        echo "NO PYTHON3"
+    else
+        echo "PYTHON3 FOUND!"
+        python3 --version
+    fi
 }
 
 main
