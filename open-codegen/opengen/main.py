2c3558e600367ac3af891810fc754b80e61c58de
Pantelis Sopasakis <p.sopasakis@gmail.com>
2019-06-01 16:47:24 +0100

    :snake: fix python2.7 issue

--- open-codegen/opengen/main.py
+++ open-codegen/opengen/main.py
@@ -1,47 +1,4 @@
-import casadi.casadi as cs
 import opengen as og
 
-u = cs.SX.sym("u", 5)
-p = cs.SX.sym("p", 2)
-phi = og.functions.rosenbrock(u, p)
-c = cs.vertcat(1.5*u[0] - u[1],
-               u[2] - u[3],
-               cs.cos(u[4]))
-xmin = [-2., -1., -1., -1., -4.]
-xmax = [1., 3., 1., 1., 4.]
-
-bounds = og.constraints.Rectangle(xmin, xmax)
-
-problem = og.builder.Problem(u, p, phi) \
-    .with_penalty_constraints(c).with_constraints(bounds)
-
-
-meta = og.config.OptimizerMeta() \
-    .with_version("0.0.2") \
-    .with_authors(["P. Sopasakis", "E. Fresk"]) \
-    .with_licence("CC4.0-By") \
-    .with_optimizer_name("yo_optimizer")
-
-build_config = og.config.BuildConfiguration() \
-    .with_rebuild(False) \
-    .with_build_mode("debug") \
-    .with_build_directory(".python_codegen_builds") \
-    .with_open_version("0.3.2")
-
-solver_config = og.config.SolverConfiguration() \
-    .with_lfbgs_memory(15) \
-    .with_tolerance(1e-5) \
-    .with_max_inner_iterations(1000) \
-    .with_constraints_tolerance(1e-4) \
-    .with_max_outer_iterations(25) \
-    .with_penalty_weight_update_factor(5) \
-    .with_initial_penalty_weights(2.0) \
-    .with_max_duration_micros(3000000)
-
-builder = og.builder.OpEnOptimizerBuilder(problem,
-                                          metadata=meta,
-                                          build_configuration=build_config,
-                                          solver_configuration=solver_config) \
-    .with_generate_not_build_flag(False)
-builder.enable_tcp_interface()
-builder.build()
+mng = og.tcp.OptimizerTcpManager('.python_test_build/zzz_optimizer')
+mng.start()
--- open-codegen/opengen/tcp/optimizer_tcp_manager.py
+++ open-codegen/opengen/tcp/optimizer_tcp_manager.py
@@ -12,7 +12,6 @@ class OptimizerTcpManager:
     def __init__(self, optimizer_path):
         self.__optimizer_path = optimizer_path
         self.__tcp_details = None
-        self.__socket = None
 
     def __load_tcp_details(self):
         yaml_file = os.path.join(self.__optimizer_path, "optimizer.yml")
@@ -24,25 +23,41 @@ class OptimizerTcpManager:
         p = subprocess.Popen(command, cwd=self.__optimizer_path)
         p.wait()
 
+    @retry(tries=10, delay=1)
+    def __obtain_socket_connection(self):
+        print("connecting...")
+        tcp_data = self.__tcp_details
+        ip = tcp_data['tcp']['ip']
+        port = tcp_data['tcp']['port']
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip, port))
+        print("CONNECTED :-)")
+        return s
+
+    def __ping(self, s):
+        ping_message = b'{"Ping" : 1}'
+        s.sendall(ping_message)
+        data = s.recv(256)  # 256 is more than enough
+        return data.decode()
+
+    def ping(self):
+        with self.__obtain_socket_connection() as s:
+            data = self.__ping(s)
+        return data
+
     def start(self):
         self.__load_tcp_details()
         thread = Thread(target=self.__threaded_start)
+
+        # start the server
         thread.start()
-        time.sleep(2)
 
-    @retry(ConnectionRefusedError, tries=1, delay=2)
-    def __obtain_socket_connection(self, ip, port):
-        print("connecting...")
-        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.__socket.connect((ip, port))
-        print("CONNECTED :-)")
+        # ping the server until it responds so that we know it's
+        # up and running
+        pong = self.ping()
+        print(pong)
 
-    def connect(self):
-        tcp_details = self.__tcp_details
-        ip = tcp_details['tcp']['ip']
-        port = tcp_details['tcp']['port']
-        self.__obtain_socket_connection(ip, port)
-        self.__socket.listen(1)
+    def call(self, p):
+        em = b'{"Run" : {"parameter", [1.0, 2.0]}}'
 
-
-
+        # ---- WIP
