d7ac76abbee48f69a683dad18010b27f1fc3a987
Pantelis Sopasakis <p.sopasakis@gmail.com>
2019-09-20 18:53:11 +0100

    problem.py: new problem setup

--- open-codegen/opengen/builder/problem.py
+++ open-codegen/opengen/builder/problem.py
@@ -5,16 +5,15 @@ class Problem:
     """Definition of an optimization problem
 
     Provides the cost function, constraints and additional
-    penalty-type constraints.
+    ALM/penalty-type constraints.
     """
 
     def __init__(self, u, p, cost):
         """Construct an optimization problem
 
-        Args:
-            u: decision variable (CasADi variable)
-            p: parameter (CasADi variable)
-            cost: cost function (CasADi function of u and p)
+            :param u: decision variable (CasADi variable)
+            :param p: parameter (CasADi variable)
+            :param cost: cost function (CasADi function of u and p)
 
         Example:
             >>> import casadi.casadi as cs
@@ -28,13 +27,27 @@ class Problem:
             >>> problem = og.builder.Problem(u, p, phi)
 
         """
+
+        # Decision variable: u
         self.__u = u
+        # Parameter: p
         self.__p = p
+        # Cost function: f(u, p)
         self.__cost = cost
+        # Constraints on u: u in U
         self.__u_constraints = None
-        self.__penalty_constraints = None
+        # ALM-type constraints: mapping F1(u, p)
+        self.__alm_mapping_f1 = None
+        # ALM-type constraints: set C
+        # Corresponds to the constraints: F1(u, p) in C
+        self.__alm_set_c = None
+        # Set Y of dual variables (compact subset of C*)
+        self.__alm_set_y = None
+        # Penalty-type constraints: mapping F2(u, p)
+        # Constraints: F1(u, p) = 0
+        self.__penalty_mapping_f2 = None
+        # @deprecated - to be removed
         self.__penalty_function = None
-        self.__al_constraints = None
 
     # ---------- SETTERS -----------------------------------------------
 
@@ -43,9 +56,9 @@ class Problem:
 
         Args:
             u_constraints: constraints on the decision variable; must
-            be a Constraint object (such as
-            \link opengen.constraints.ball2.Ball2 Ball2 \endlink
-            and \link opengen.constraints.rectangle.Rectangle Rectangle \endlink)
+                           be a Constraint object (such as
+                           opengen.constraints.ball2.Ball2
+                           and opengen.constraints.rectangle.Rectangle)
 
         Returns:
             Current object
@@ -61,23 +74,23 @@ class Problem:
         function c(u; p)) and the penalty function, g. If no penalty function
         is specified, the quadratic penalty will be used.
 
-        Args:
-            penalty_constraints: a function <code>c(u, p)</code>, of the decision
-            variable <code>u</code> and the parameter vector <code>p</code>, which
-            corresponds to the constraints <code>c(u, p)</code>
-            penalty_function: a function <code>g: R -> R</code>, used to define the
-            penalty in the penalty method; the default is <code>g(z) = z^2</code>.
-            You typically will not need to change this, but if you very much want to,
-            you need to provide an instance of <code>casadi.casadi.Function</code>
-            (not a CasADi symbol such as <code>SX</code>).
 
-        Returns:
-            self
+        :param penalty_constraints: a function <code>c(u, p)</code>, of the decision
+                variable <code>u</code> and the parameter vector <code>p</code>, which
+                corresponds to the constraints <code>c(u, p)</code>
+
+        :param penalty_function: a function <code>g: R -> R</code>, used to define the
+                penalty in the penalty method; the default is <code>g(z) = z^2</code>.
+                You typically will not need to change this, but if you very much want to,
+                you need to provide an instance of <code>casadi.casadi.Function</code>
+                (not a CasADi symbol such as <code>SX</code>).
+
+        :return: self
         """
         if penalty_constraints is None:
             pass
 
-        self.__penalty_constraints = penalty_constraints
+        self.__penalty_mapping_f2 = penalty_constraints
         if penalty_function is None:
             # default penalty function: quadratic
             z = cs.SX.sym("z")
@@ -86,14 +99,28 @@ class Problem:
             self.__penalty_function = penalty_function
         return self
 
-    def with_aug_lagrangian_constraints(self, al_constraints):
-        """Not implemented yet"""
-        raise NotImplementedError
+    def with_aug_lagrangian_constraints(self, mapping_f1, set_c, set_y):
+        """
+        Constraints F1(u, p) in C
+
+        :param mapping_f1:
+        :param set_c:
+        :param set_y:
+
+        :return: self
+        """
+        self.__alm_mapping_f1 = mapping_f1
+        self.__alm_set_c = set_c
+        self.__alm_set_y = set_y
+        raise self
 
     # ---------- DIMENSIONS --------------------------------------------
 
     def dim_decision_variables(self):
-        """Number of decision variables"""
+        """Number of decision variables
+
+        :return: number of decision variables
+        """
         return self.__u.size(1)
 
     def dim_parameters(self):
@@ -102,11 +129,13 @@ class Problem:
 
     def dim_constraints_penalty(self):
         """Number of penalty-type constraints"""
-        return 0 if self.__penalty_constraints is None \
-            else self.__penalty_constraints.size(1)
+        return 0 if self.__penalty_mapping_f2 is None \
+            else self.__penalty_mapping_f2.size(1)
 
     def dim_constraints_aug_lagrangian(self):
         """Not implemented yet"""
+        return 0 if self.__alm_mapping_f1 is None \
+            else self.__alm_mapping_f1.size(1)
         return 0
 
     # ---------- OTHER GETTERS -----------------------------------------
@@ -117,24 +146,34 @@ class Problem:
         return self.__cost
 
     @property
-    def penalty_constraints(self):
-        """Penalty constraints as a CasADi symbol (function c)"""
-        return self.__penalty_constraints
+    def penalty_mapping_f2(self):
+        """Penalty-type mapping F2 as a CasADi symbol"""
+        return self.__penalty_mapping_f2
+
+    @property
+    def penalty_mapping_f1(self):
+        """ALM mapping F1 as a CasADi symbol"""
+        return self.__alm_mapping_f1
+
+    @property
+    def alm_set_c(self):
+        """Set C in the definition of constraints: F1(u, p) in C"""
+        return self.__alm_set_c
+
+    @property
+    def alm_set_y(self):
+        """Set Y for the Lagrange multipliers"""
+        return self.__alm_set_y
 
     @property
     def penalty_function(self):
-        """Penalty function, <code>g</code>"""
+        """Penalty function, g"""
         return self.__penalty_function
 
     @property
     def constraints(self):
-        """Hard constraints"""
+        """Hard constraints; set U"""
         return self.__u_constraints
-
-    @property
-    def constraints_aug_lagrangian(self):
-        """Not implemented yet"""
-        raise NotImplementedError
 
     @property
     def decision_variables(self):
