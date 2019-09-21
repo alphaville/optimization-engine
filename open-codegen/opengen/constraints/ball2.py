747ddfc9efd4dabaa66b39e93ebc787b8241bc89
Pantelis Sopasakis <p.sopasakis@gmail.com>
2019-09-21 13:02:56 +0100

    python: squared distance to ball-2 support for lists, np.ndarray and SX

--- open-codegen/opengen/constraints/ball2.py
+++ open-codegen/opengen/constraints/ball2.py
@@ -1,3 +1,7 @@
+import casadi.casadi as cs
+import numpy as np
+
+
 class Ball2:
     """A Euclidean ball constraint
 
@@ -36,4 +40,41 @@ class Ball2:
     def radius(self):
         """Returns the radius of the ball"""
         return self.__radius
+
+    def distance_squared(self, u):
+        """Computes the squared distance between a given point `u` and this ball
+
+            :param u: given point; can be a list of float, a numpy
+            ndarray or a CasADi SX symbol
+
+            :return: distance from set as a float or a CasADi symbol
+        """
+        # Function `distance` can be applied to CasADi symbols and
+        # lists of numbers. However, if `u` is a symbol, we need to
+        # use appropriate CasADi functions like cs.sign and cs.norm_2
+        if isinstance(u, cs.SX):
+            # Case I: `u` is a CasADi SX symbol
+            sign_fun = cs.sign
+            max_fun = cs.fmax
+            norm_fun = cs.norm_2
+            v = u if self.__center is None else u - self.__center
+        elif isinstance(u, list) and all(isinstance(x, (int, float)) for x in u)\
+                or isinstance(u, np.ndarray):
+            # Case II: `u` is an array of numbers or an np.ndarray
+            sign_fun = np.sign
+            norm_fun = np.linalg.norm
+            max_fun = np.fmax
+            if self.__center is None:
+                v = u
+            else:
+                z = np.array(self.__center).reshape((len(u), 1))
+                u = np.array(u).reshape((len(u), 1))
+                v = np.subtract(u, z)
+        else:
+            raise Exception("u is of invalid type")
+
+        # Compute distance
+        t = norm_fun(v) - self.radius
+        return max_fun(0.0, sign_fun(t) * t ** 2)
+
 
