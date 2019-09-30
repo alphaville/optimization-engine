1d69382083c9bb6bd32dd3ee10a9555d68af918b
Pantelis Sopasakis <p.sopasakis@gmail.com>
2019-09-30 16:18:37 +0100

    testing C bindings from Python

--- open-codegen/opengen/test/test.py
+++ open-codegen/opengen/test/test.py
@@ -227,101 +227,45 @@ class RustBuildTestCase(unittest.TestCase):
 
         mng.kill()
 
-    # def test_link_2_c_libs(self):
-    #     u = cs.SX.sym("u", 5)
-    #     p = cs.SX.sym("p", 2)
-    #     phi = og.functions.rosenbrock(u, p)
-    #     c = cs.vertcat(1.5*u[0] - u[1], u[2] - u[3])
-    #     xmin = [-2.0, -2.0, -2.0, -2.0, -2.0]
-    #     xmax = [ 2.0,  2.0,  2.0,  2.0,  2.0]
-    #     bounds = og.constraints.Rectangle(xmin, xmax)
-    #     problem = og.builder.Problem(u, p, phi) \
-    #         .with_penalty_constraints(c)        \
-    #         .with_constraints(bounds)
-    #     meta1 = og.config.OptimizerMeta()               \
-    #         .with_version("0.0.0")                      \
-    #         .with_authors(["P. Sopasakis", "E. Fresk"]) \
-    #         .with_licence("CC4.0-By")                   \
-    #         .with_optimizer_name("the_optimizer1")
-    #     meta2 = og.config.OptimizerMeta()               \
-    #         .with_version("0.0.0")                      \
-    #         .with_authors(["P. Sopasakis", "E. Fresk"]) \
-    #         .with_licence("CC4.0-By")                   \
-    #         .with_optimizer_name("the_optimizer2")
-    #     build_config = og.config.BuildConfiguration()          \
-    #         .with_rebuild(False)                               \
-    #         .with_build_mode("debug")                          \
-    #         .with_build_directory(RustBuildTestCase.TEST_DIR)  \
-    #         .with_build_c_bindings()
-    #     solver_config = og.config.SolverConfiguration()   \
-    #         .with_lfbgs_memory(15)                        \
-    #         .with_tolerance(1e-5)                         \
-    #         .with_max_inner_iterations(155)               \
-    #         .with_delta_tolerance(1e-4)             \
-    #         .with_max_outer_iterations(15)                \
-    #         .with_penalty_weight_update_factor(8.0)       \
-    #         .with_initial_penalty_weights([20.0, 5.0])
-    #
-    #     builder1 = og.builder.OpEnOptimizerBuilder(problem,
-    #                                               metadata=meta1,
-    #                                               build_configuration=build_config,
-    #                                               solver_configuration=solver_config) \
-    #         .with_generate_not_build_flag(False).with_verbosity_level(0)
-    #     builder1.build()
-    #
-    #     builder2 = og.builder.OpEnOptimizerBuilder(problem,
-    #                                               metadata=meta2,
-    #                                               build_configuration=build_config,
-    #                                               solver_configuration=solver_config) \
-    #         .with_generate_not_build_flag(False).with_verbosity_level(0)
-    #     builder2.build()
-    #
-    #     p = subprocess.Popen(["gcc",
-    #         "test/test_2_solvers.c",
-    #         "-I" + RustBuildTestCase.TEST_DIR + "/the_optimizer1",
-    #         "-I" + RustBuildTestCase.TEST_DIR + "/the_optimizer2",
-    #         "-pthread",
-    #         RustBuildTestCase.TEST_DIR + "/the_optimizer1/target/debug/libthe_optimizer1.a",
-    #         RustBuildTestCase.TEST_DIR + "/the_optimizer2/target/debug/libthe_optimizer2.a",
-    #         "-lm",
-    #         "-ldl",
-    #         "-std=c99",
-    #         "-o" + RustBuildTestCase.TEST_DIR + "/test_2_solvers"],
-    #         #stdin=subprocess.PIPE,
-    #         stdout=subprocess.PIPE,
-    #         stderr=subprocess.PIPE)
-    #
-    #     output, err = p.communicate()
-    #     rc = p.returncode
-    #
-    #     print("gcc output: ")
-    #     print(output.decode("utf-8"))
-    #     print("gcc err: ")
-    #     print(err.decode("utf-8"))
-    #     print("gcc rc: ")
-    #     print(rc)
-    #
-    #     if rc != 0:
-    #         exit(rc)
-    #
-    #     p = subprocess.Popen(["./" + RustBuildTestCase.TEST_DIR + "/test_2_solvers"],
-    #         #stdin=subprocess.PIPE,
-    #         stdout=subprocess.PIPE,
-    #         stderr=subprocess.PIPE)
-    #
-    #     output, err = p.communicate()
-    #     rc = p.returncode
-    #
-    #     print("test_2_solvers output: ")
-    #     print(output.decode("utf-8"))
-    #     print("test_2_solvers err: ")
-    #     print(err.decode("utf-8"))
-    #     print("test_2_solvers rc: ")
-    #     print(rc)
-    #
-    #     if rc != 0:
-    #         exit(rc)
-    #
+    @staticmethod
+    def c_bindings_helper(optimizer_name):
+        p = subprocess.Popen(["gcc",
+                              RustBuildTestCase.TEST_DIR + "/" + optimizer_name + "/example_optimizer.c",
+                              "-I" + RustBuildTestCase.TEST_DIR + "/" + optimizer_name,
+                              "-pthread",
+                              RustBuildTestCase.TEST_DIR + "/" + optimizer_name + "/target/debug/lib" + optimizer_name + ".a",
+                              "-lm",
+                              "-ldl",
+                              "-std=c99",
+                              "-o",
+                              RustBuildTestCase.TEST_DIR + "/" + optimizer_name + "/optimizer"],
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+
+        # Make sure it compiles
+        p.communicate()
+        rc1 = p.returncode
+
+        # Run the optimizer
+        p = subprocess.Popen([RustBuildTestCase.TEST_DIR + "/" + optimizer_name + "/optimizer"],
+                             stdout=subprocess.DEVNULL)
+        p.communicate()
+        rc2 = p.returncode
+
+        return rc1, rc2
+
+    def test_c_bindings(self):
+        rc1, rc2 = RustBuildTestCase.c_bindings_helper(optimizer_name="only_f1")
+        self.assertEqual(0, rc1)
+        self.assertEqual(0, rc2)
+
+        rc1, rc2 = RustBuildTestCase.c_bindings_helper(optimizer_name="only_f2")
+        self.assertEqual(0, rc1)
+        self.assertEqual(0, rc2)
+
+        rc1, rc2 = RustBuildTestCase.c_bindings_helper(optimizer_name="plain")
+        self.assertEqual(0, rc1)
+        self.assertEqual(0, rc2)
 
 
 if __name__ == '__main__':
