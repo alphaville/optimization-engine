fc89f7179cbc4b7c7208f381f688b68fdb308211
Pantelis Sopasakis <p.sopasakis@gmail.com>
2020-04-12 16:47:27 +0100

    trying to fix travis OSX issue removed rustup target add

--- ci/install.sh
+++ ci/install.sh
@@ -2,7 +2,7 @@ set -euxo pipefail
 
 main() {
     if [ $TARGET != x86_64-unknown-linux-gnu ]; then
-        rustup target add $TARGET
+#        rustup target add $TARGET
     fi
     sudo pip install --upgrade pip
     sudo pip install virtualenv --upgrade --ignore-installed six
