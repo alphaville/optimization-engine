4c973f268e0619c04d00862c395d6aab08b04089
Pantelis Sopasakis <p.sopasakis@gmail.com>
2020-04-15 21:31:00 +0100

    double brackets: trying again (travis)

--- ci/install.sh
+++ ci/install.sh
@@ -2,7 +2,7 @@ set -euxo pipefail
 
 main() {
     # If DO_DOCKER is unset, install necessary stuff for testing
-    if [[ -z ${DO_DOCKER+x} ]]; then
+    if [ -z $DO_DOCKER ]; then
         rustup toolchain remove stable && rustup toolchain install stable
         sudo pip install --upgrade pip
         sudo pip install virtualenv --upgrade --ignore-installed six
