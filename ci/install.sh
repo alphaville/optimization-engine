f4763ab8a47bc72a809f8a46cbdd5cd7ab17bb80
Pantelis Sopasakis <p.sopasakis@gmail.com>
2020-04-15 21:24:23 +0100

    travis: removed double then keywrd

--- ci/install.sh
+++ ci/install.sh
@@ -3,7 +3,6 @@ set -euxo pipefail
 main() {
     # If DO_DOCKER is unset, install necessary stuff for testing
     if [ -z ${DO_DOCKER+x} ]; then
-    then
         rustup toolchain remove stable && rustup toolchain install stable
         sudo pip install --upgrade pip
         sudo pip install virtualenv --upgrade --ignore-installed six
