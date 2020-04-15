f0cc93dddc8cf232d49f70af9cb3541b44d12066
Pantelis Sopasakis <p.sopasakis@gmail.com>
2020-04-15 21:14:42 +0100

    bloody travis

--- ci/install.sh
+++ ci/install.sh
@@ -3,10 +3,9 @@ set -euxo pipefail
 main() {
     if [ -v ${DO_DOCKER+set} ]
     then
-        #rustup toolchain remove stable && rustup toolchain install stable
-        #sudo pip install --upgrade pip
-        #sudo pip install virtualenv --upgrade --ignore-installed six
-
+        rustup toolchain remove stable && rustup toolchain install stable
+        sudo pip install --upgrade pip
+        sudo pip install virtualenv --upgrade --ignore-installed six
     fi
 }
 
