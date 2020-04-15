714500e1aa5a7f95d60f962eb4ce40488c4cf40a
Pantelis Sopasakis <alphaville@users.noreply.github.com>
2020-04-15 22:19:22 +0100

    Docker testing on Travis CI: configuration (#168)
    Docker on Travis CI
    added job that builds docker image

--- ci/install.sh
+++ ci/install.sh
@@ -1,9 +1,12 @@
 set -euxo pipefail
 
 main() {
-    rustup toolchain remove stable && rustup toolchain install stable
-    sudo pip install --upgrade pip
-    sudo pip install virtualenv --upgrade --ignore-installed six
+    # Install necessary stuff
+    if [ $DO_DOCKER -eq 0 ]; then
+        rustup toolchain remove stable && rustup toolchain install stable
+        sudo pip install --upgrade pip
+        sudo pip install virtualenv --upgrade --ignore-installed six
+    fi
 }
 
 main
