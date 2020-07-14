284b9c725249fdc712ddb0358c6d9ef099a46bd1
Pantelis Sopasakis <p.sopasakis@gmail.com>
2020-07-14 02:09:47 +0100

    updating cartesian.py

--- open-codegen/CHANGELOG.md
+++ open-codegen/CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 
 Note: This is the Changelog file of `opengen` - the Python interface of OpEn
 
+## [0.6.0]
+
+### Changed
+
+* 
+
 ## [0.5.0]
 
 ### Fixed
@@ -41,5 +47,6 @@ Note: This is the Changelog file of `opengen` - the Python interface of OpEn
 ### Removed
 
 
+[0.6.0]: https://github.com/alphaville/optimization-engine/compare/opengen-0.6.0...opengen-0.5.0
 [0.5.0]: https://github.com/alphaville/optimization-engine/compare/opengen-0.5.0...opengen-0.4.1
 [0.4.1]: https://github.com/alphaville/optimization-engine/compare/opengen-0.4.1...master
--- open-codegen/opengen/constraints/cartesian.py
+++ open-codegen/opengen/constraints/cartesian.py
@@ -5,7 +5,26 @@ from typing import List
 class CartesianProduct(constraint.Constraint):
 
     def __init__(self, dimension: int, segments: List[int], constraints: List[constraint.Constraint]):
-        # TODO: Check whether input arguments are valid (types, ranges, etc)
+        """
+        Construct a Cartesian product of constraints by providing a list of sets
+        and their dimensions as follows: an n-dimensional vector x can be partitioned
+        into subvectors as x = (x1, x2, ..., xs), where each xi has dimension mi.
+        For example consider the 5-dimensional vector x = (x_0, x_1, x_2, x_3, x_4),
+        which can be partitioned into x1 = (x_0, x_1) and x2 = (x_2, x_3, x_4).
+        We can associate with x1 the indices [0, 1] and with x2 the indices [2, 3, 4].
+        The *segment ids* are the indices 1 and 4.
+
+        Example:
+        > ball = og.constraints.Ball2(None, 1.5)
+        > rect = og.constraints.Rectangle(xmin=[-1,-2,-3], xmax=[0, 10, -1])
+        > free = og.constraints.NoConstraints()
+        > segment_ids = [1, 4, 9]
+        > dim = 10
+
+        :param dimension: dimension of the Cartesian product
+        :param segments: ids of segments
+        :param constraints: list of sets
+        """
         self.__dimension = dimension
         self.__segments = segments
         self.__constraints = constraints
