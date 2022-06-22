002f82614ee181dc604cc916fd8d0fc1429b8984
Pantelis Sopasakis <p.sopasakis@gmail.com>
2022-06-22 19:18:32 +0100

    new test for PM

--- src/alm/tests.rs
+++ src/alm/tests.rs
@@ -1,7 +1,7 @@
 use crate::{
     alm::*,
     core::{constraints::*, panoc::*, ExitStatus},
-    mocks, FunctionCallResult,
+    matrix_operations, mocks, FunctionCallResult, SolverError,
 };
 
 #[test]
@@ -154,6 +154,84 @@ fn t_create_alm_optimizer() {
 
     let mut u = vec![0.0; nx];
     println!("result = {:?}", alm_optimizer.solve(&mut u));
+}
+
+#[test]
+fn t_alm_only_penalty_method() {
+    let tolerance = 1e-4;
+    let nx = 3;
+    let n1 = 0;
+    let n2 = 1;
+    let lbfgs_mem = 5;
+    let panoc_cache = PANOCCache::new(nx, tolerance, lbfgs_mem);
+    let mut alm_cache = AlmCache::new(panoc_cache, n1, n2);
+
+    let bounds = NoConstraints::new();
+
+    let f = |u: &[f64], cost: &mut f64| -> Result<(), SolverError> {
+        *cost = 0.5 * matrix_operations::norm2_squared(u) + matrix_operations::sum(u);
+        Ok(())
+    };
+
+    let df = |u: &[f64], grad: &mut [f64]| -> Result<(), SolverError> {
+        grad.iter_mut()
+            .zip(u.iter())
+            .for_each(|(grad_i, u_i)| *grad_i = u_i + 1.0);
+        Ok(())
+    };
+
+    let f2 = |u: &[f64], res: &mut [f64]| -> Result<(), SolverError> {
+        res[0] = matrix_operations::norm2_squared(u) - 1.;
+        Ok(())
+    };
+    let jf2t = |u: &[f64], d: &[f64], res: &mut [f64]| -> Result<(), crate::SolverError> {
+        res.iter_mut()
+            .zip(u.iter())
+            .for_each(|(res_i, u_i)| *res_i = u_i * d[0]);
+        Ok(())
+    };
+
+    let factory = AlmFactory::new(
+        f,
+        df,
+        NO_MAPPING,
+        NO_JACOBIAN_MAPPING,
+        Some(f2),
+        Some(jf2t),
+        NO_SET,
+        n2,
+    );
+
+    let alm_problem = AlmProblem::new(
+        bounds,
+        NO_SET,
+        NO_SET,
+        |u: &[f64], xi: &[f64], cost: &mut f64| -> Result<(), SolverError> {
+            factory.psi(u, xi, cost)
+        },
+        |u: &[f64], xi: &[f64], grad: &mut [f64]| -> Result<(), SolverError> {
+            factory.d_psi(u, xi, grad)
+        },
+        NO_MAPPING,
+        Some(f2),
+        n1,
+        n2,
+    );
+
+    let mut alm_optimizer = AlmOptimizer::new(&mut alm_cache, alm_problem)
+        .with_delta_tolerance(1e-6)
+        .with_epsilon_tolerance(1e-5)
+        .with_max_outer_iterations(20)
+        .with_max_inner_iterations(1000)
+        .with_initial_penalty(5000.0)
+        .with_penalty_update_factor(2.2);
+
+    let mut u = vec![0.1; nx];
+    let solver_result = alm_optimizer.solve(&mut u);
+    assert!(solver_result.is_ok());
+    let r = solver_result.unwrap();
+    assert!(r.exit_status() == ExitStatus::Converged);
+    assert!(r.f2_norm() < 1e-6);
 }
 
 #[test]
