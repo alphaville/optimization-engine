17e4f12c24425796e432d57ab3380a9ed8f723a7
Pantelis Sopasakis <p.sopasakis@gmail.com>
2022-11-26 16:28:08 +0000

    [docit] try now

--- ci/sphinx-documentation.sh
+++ ci/sphinx-documentation.sh
@@ -16,7 +16,7 @@ echo "COMMIT HASH   :" $commit_hash
 # If the current branch is not master and the current commit
 # message does not contain [docit], then stop
 magic_docs_keyword="[docit]"
-if [[ "$commit_message" != *"$magic_docs_keyword"* ]] || ["$current_branch" == "master"]; then 
+if [[ "$commit_message" != *"$magic_docs_keyword"* ]] || [[ "$current_branch" == "master" ]]; then 
 echo "The commit message does not contain [docit] and this is not the master branch /exiting! bye :)";
 exit 0;
 fi
