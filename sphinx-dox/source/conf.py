5016919943ae8a038061329896d3164bb29a4116
Pantelis Sopasakis <p.sopasakis@gmail.com>
2022-11-26 12:11:06 +0000

    sphinx-apidoc and new theme

--- sphinx-dox/source/conf.py
+++ sphinx-dox/source/conf.py
@@ -29,5 +29,5 @@ exclude_patterns = ['test/*']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
