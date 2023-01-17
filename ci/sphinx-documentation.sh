05949d715b13af635f23a8d6e8d5ee651419f914
Pantelis Sopasakis <p.sopasakis@gmail.com>
2023-01-17 21:30:30 +0000

    [docit] Tue 17 Jan 2023 21:30:30 UTC to Tue 17 Jan 2023 21:30:30 UTC

--- ci/sphinx-documentation.sh
+++ ci/sphinx-documentation.sh
@@ -50,7 +50,7 @@ rm -rf sphinx
 mkdir -p sphinx
 pushd sphinx-dox
 sphinx-apidoc -o ./source/ ../open-codegen/opengen
-echo Last updated: `date -u`  >> source/index.rst; sed '$d' source/index.rst; # update date at the end of file
+echo Last updated: $(date -u)  >> source/index.rst; sed '$d' source/index.rst; # update date at the end of file
 make html || :
 cp -r build/html/ ../sphinx
 git checkout source/index.rst # no need to commit this
