23836b025d635a5b5fc40c26e6e8fdfa605fec80
Pantelis Sopasakis <p.sopasakis@gmail.com>
2024-08-15 14:13:42 +0100

    [docit] affine space: python docstrings

--- open-codegen/opengen/constraints/affine_space.py
+++ open-codegen/opengen/constraints/affine_space.py
@@ -21,20 +21,36 @@ class AffineSpace(Constraint):
 
     @property
     def matrix_a(self):
+        """Matrix A
+        """
         return self.__A
 
     @property
     def vector_b(self):
+        """Vector b        
+        """
         return self.__b
 
     def distance_squared(self, u):
+        """Squared distance to affine space
+
+        Not implemented yet
+        """
         raise NotImplementedError()
 
     def project(self, u):
+        """Projection on affine space
+
+        Not implemented yet
+        """
         raise NotImplementedError()
 
     def is_convex(self):
+        """Affine spaces are convex sets
+        """
         return True
 
     def is_compact(self):
+        """Affine spaces are not compact sets
+        """
         return False
